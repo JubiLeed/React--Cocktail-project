08c8e705f17e1163932b0521c6d2e8738b894869
JubiLeed <vincehong323@gmail.com>
2023-03-10 15:03:26 -0600

    setup gh-pages

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, sequi.
+        Eaque nihil dicta corporis, eius aspernatur aliquam adipisci voluptates
+        quaerat nostrum corrupti aut quam dolor inventore a sit debitis
+        laboriosam rem similique hic? Natus molestias, nobis quasi minus velit
+        incidunt!
+      </p>
+    </section>
+  );
+};
+
+export default About;
